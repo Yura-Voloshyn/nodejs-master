7b08062dc9faa5698442006053c2006c79ead31b
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-11-16 20:50:00 +0200

    verify email task done

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,10 @@
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
 const { User } = require("../../models/user");
 
-const { RequestError } = require("../../helpers");
+const { RequestError, sendEmail, createVerifyEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { password, email, subscription } = req.body;
@@ -13,12 +14,16 @@ const register = async (req, res) => {
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
   const result = await User.create({
     password: hashPassword,
     email,
     subscription,
     avatarURL,
+    verificationToken,
   });
+  const mail = createVerifyEmail(email, verificationToken);
+  await sendEmail(mail);
   res.status(201).json({
     email: result.email,
     subscription: result.subscription,
